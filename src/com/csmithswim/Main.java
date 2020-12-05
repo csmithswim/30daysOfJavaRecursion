c3060b14c89de5787abf2ab6a09cf6fd0d55e68d
Cullen S <cullensmi@gmail.com>
2020-12-05 14:35:00 -0500

    solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,22 +1,16 @@
 package com.csmithswim;
 
+
 import java.util.Scanner;
+
 
 public class Main {
 
+    //Notes to consider: The end condition is when the recursive call stops calling itself and it should be a conditional that is an ==.
+    // Also known as a base case
     public static int factorial(int n){
-
-
-        // factorial = n * (n -1)...
-        n=n*factorial(n);
-
-        if (n <= 0) {
-
-            return n;
-
-        }
-        return factorial(n-1);
-
+        if (n == 1) return 1;
+        else return n * factorial(n-1);
     }
 
     public static void main(String[] args) {
